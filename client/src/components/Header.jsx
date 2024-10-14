5b0ea288cb2462df8fe6514f782ab6a2a0cee319
Daniel Watchman <dannietortee@gmail.com>
2024-10-14 13:26:19 +0000

    add hamburger

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,10 @@
-import { Button, Navbar, TextInput } from 'flowbite-react'
-import { Link } from 'react-router-dom'
+import { Button, Navbar, NavbarToggle, TextInput } from 'flowbite-react'
+import { Link, useLocation } from 'react-router-dom'
 import { AiOutlineSearch } from 'react-icons/ai'
 import { FaMoon } from 'react-icons/fa'
 
 export default function Header() {
+    const path = useLocation().pathname;
   return (
     <Navbar className='border-b-2'>
       <Link to='/' className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
@@ -30,7 +31,25 @@ export default function Header() {
                 Sign in
             </Button>
         </Link>
+        <Navbar.Toggle></Navbar.Toggle>
       </div>
+        <Navbar.Collapse>
+            <Navbar.Link active={path === '/'} as={'div'}>
+                <Link to='/'>
+                    Home
+                </Link>
+            </Navbar.Link>
+            <Navbar.Link active={path === '/about'} as={'div'}>
+                <Link to='/about'>
+                    About
+                </Link>
+            </Navbar.Link>
+            <Navbar.Link active={path === '/projects'} as={'div'}>
+                <Link to='/projects'>
+                    Projects
+                </Link>
+            </Navbar.Link>
+        </Navbar.Collapse>
     </Navbar>
   )
 }
