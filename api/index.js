ec7ccbadae7c80c37e1f65595b7180a1e8750a0c
Daniel Watchman <dannietortee@gmail.com>
2024-10-14 22:39:16 +0000

    update index.js

--- api/index.js
+++ api/index.js
@@ -19,4 +19,8 @@ const app = express();
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
-})
+});
+
+app.get('/test', (req, res) => {
+    res.json({message: 'API is working!'});
+}); 
