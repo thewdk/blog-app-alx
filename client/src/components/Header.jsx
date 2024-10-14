30edb1d71dd8ee8e80dba11f231fcb54f6795451
Daniel Watchman <dannietortee@gmail.com>
2024-10-14 12:48:28 +0000

    setup react icon and work on Header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import { Button, Navbar, TextInput } from 'flowbite-react'
+import { Link } from 'react-router-dom'
+import { AiOutlineSearch } from 'react-icons/ai'
+import { FaMoon } from 'react-icons/fa'
 
 export default function Header() {
   return (
-    <div>
-      header
-    </div>
+    <Navbar className='border-b-2'>
+      <Link to='/' className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
+        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>The O2</span>
+        Blog
+      </Link>
+      <form action="">
+        <TextInput 
+            type='text'
+            placeholder='Search...'
+            rightIcon={AiOutlineSearch} 
+            className='hidden lg:inline'
+        />
+      </form>
+      <Button className='w-12 h-10 lg:hidden' color='gray' pill>
+        <AiOutlineSearch />
+      </Button>
+      <div className=''>
+        <Button className='w-12 h-10'>
+            <FaMoon />
+        </Button>
+      </div>
+    </Navbar>
   )
 }
