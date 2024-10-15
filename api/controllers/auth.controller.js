35e863c4968de4f9385ce4523d935b5c224cd799
Daniel Watchman <dannietortee@gmail.com>
2024-10-15 10:39:16 +0000

    Setup middleware

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
     const { username, email, password } = req.body;
 
     if (!username || !email || !password || username === '' || email === '' || password === '') {
@@ -19,7 +19,8 @@ export const signup = async (req, res) => {
         await newUser.save();
         res.json('Signup successful');
     } catch (error) {
-        res.status(500).json({message: error.message});
+        next(error);
+        // res.status(500).json({message: error.message});
         
     }
 
