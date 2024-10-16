7f8a1c06c78b4e26af1607cd92cddd468cab1a11
Daniel Watchman <dannietortee@gmail.com>
2024-10-16 01:55:59 +0000

    create Footer component

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -55,9 +55,9 @@ export default function FooterCom() {
                 </div>
             </div>
             <Footer.Divider/>
-            <div className="">
-                <Footer.Copyright href='#' by='TheDWK' year={new Date().getFullYear} />
-                <div className="">
+            <div className="w-full sm:flex sm:items-center sm:justify-between">
+                <Footer.Copyright href='#' by='TheDWK' year={new Date().getFullYear()} />
+                <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
                     <Footer.Icon href='#' icon={BsFacebook}/>
                     <Footer.Icon href='#' icon={BsInstagram}/>
                     <Footer.Icon href='#' icon={BsTwitter}/>
