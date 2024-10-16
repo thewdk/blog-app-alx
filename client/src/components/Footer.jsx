acd120071af4a2feb2180bb23371d50a04914b01
Daniel Watchman <dannietortee@gmail.com>
2024-10-16 01:49:59 +0000

    add Footer.Icon

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,10 +1,72 @@
 import { Footer } from 'flowbite-react'
-
+import { Link } from 'react-router-dom'
+import { BsFacebook, BsInstagram, BsTwitter, BsGithub } from 'react-icons/bs'
 
 export default function FooterCom() {
   return (
-    <div>
-      <Footer>footer</Footer>
-    </div>
+
+      <Footer container className='border border-t-8 border-teal-500'>
+        <div className="w-full max-w-7xl mx-auto">
+            <div className="grid w-full justify-between sm:flex md:grid-cols-1">
+                <div className="mt-5">
+                    <Link to='/' className='self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white'>
+                        <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>The O2</span>
+                        Blog
+                    </Link>
+                </div>
+                <div className=" grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6">
+                    <div>
+                        <Footer.Title title='About' />
+                        <Footer.LinkGroup col>
+                            <Footer.Link href='#' target='_blank' rel='noopener noreferrer'>
+                                Projects
+                            </Footer.Link>
+                            <Footer.Link href='#' target='_blank' rel='noopener noreferrer'>
+                                Projects
+                            </Footer.Link>
+                        </Footer.LinkGroup>
+
+                    </div>
+                    <div>
+                        <Footer.Title title='About' />
+                        <Footer.LinkGroup col>
+                            <Footer.Link href='#' target='_blank' rel='noopener noreferrer'>
+                                Projects
+                            </Footer.Link>
+                            <Footer.Link href='#' target='_blank' rel='noopener noreferrer'>
+                                Projects
+                            </Footer.Link>
+                        </Footer.LinkGroup>
+
+                    </div>
+                    <div>
+                        <Footer.Title title='Connect' />
+                        <Footer.LinkGroup col>
+                            <Footer.Link href='#' target='_blank' rel='noopener noreferrer'>
+                                Projects
+                            </Footer.Link>
+                            <Footer.Link href='#' target='_blank' rel='noopener noreferrer'>
+                                Projects
+                            </Footer.Link>
+                        </Footer.LinkGroup>
+
+                    </div>
+
+                </div>
+            </div>
+            <Footer.Divider/>
+            <div className="">
+                <Footer.Copyright href='#' by='TheDWK' year={new Date().getFullYear} />
+                <div className="">
+                    <Footer.Icon href='#' icon={BsFacebook}/>
+                    <Footer.Icon href='#' icon={BsInstagram}/>
+                    <Footer.Icon href='#' icon={BsTwitter}/>
+                    <Footer.Icon href='#' icon={BsGithub}/>
+                    {/* <Footer.Icon href='#' icon={BsFacebook}/> */}
+                </div>
+            </div>
+        </div>
+      </Footer>
+
   )
 }
