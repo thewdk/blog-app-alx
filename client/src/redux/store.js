82352320bf33fcf39058e0eb84742b7e052e55ed
Daniel Watchman <dannietortee@gmail.com>
2024-10-16 19:41:19 +0000

    complete dark mode

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,5 +1,6 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit'
 import userReducer from './user/userSlice'
+import themeReducer from './theme/themeSlice'
 import {persistReducer, persistStore} from 'redux-persist'
 import storage from 'redux-persist/lib/storage';
 
@@ -7,6 +8,7 @@ import storage from 'redux-persist/lib/storage';
 
 const rootReducer = combineReducers({
     user: userReducer,
+    theme: themeReducer,
 });
 
 const persistConfig = {
