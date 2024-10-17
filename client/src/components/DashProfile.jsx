97eeabaaa67c82e948b0cdfa2429297a14f10617
Daniel Watchman <dannietortee@gmail.com>
2024-10-17 16:20:23 +0000

    setup image upload functionality

--- client/src/components/DashProfile.jsx
+++ client/src/components/DashProfile.jsx
@@ -1,15 +1,37 @@
 import { Button, TextInput } from 'flowbite-react'
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import {useSelector} from 'react-redux'
 
 export default function DashProfile() {
     const {currentUser} = useSelector((state) => state.user)
+    const [imageFile, setImageFile] = useState(null)
+    const [imageFileUrl, setImageFileUrl] = useState(null)
+    const filePicker = useRef();
+    const handleImageChange = (e) => {
+        const file = e.target.file[0];
+        if (file) {
+            setImageFile(file);
+            setImageFileUrl(URL.createObjectURL.(file));
+
+        }
+    };
+    useEffect(() => {
+        if (imageFile) {
+            uploadImage();
+        }
+    }, [imageFile]);
+
+    const uploadImage = async () => {
+        console.log('uploading image...');
+        
+    }
   return (
     <div className='max-w-lg mx-auto p-3 w-full '>
       <h1 className='my-7 text-center font-semibold text-3xl '>Profile</h1>
       <form className='flex flex-col gap-4'>
-        <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full">
-            <img src={currentUser.profilePicture} alt="user" className='rounded-full w-full object-cover border-8 border-[lightgray]' />
+        <input type="file" accept='image/*' onChange={handleImageChange} ref={filePicker} hidden />
+        <div className="w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full" onClick={()=> filePicker.current.click()}>
+            <img src={imageFileUrl || currentUser.profilePicture} alt="user" className='rounded-full w-full object-cover border-8 border-[lightgray]' />
         </div>
         <TextInput type='text' id='username' placeholder='username' defaultValue={currentUser.username}/>
         <TextInput type='email' id='email' placeholder='email' defaultValue={currentUser.email}/>
