3acb1bad40722bfed3f901f9734fb31c8aa1172f
Daniel Watchman <dannietortee@gmail.com>
2024-10-22 20:51:26 +0000

    show post comments

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
 import {
-  createComment,
-
+  createComment, getPostComments
 } from '../controllers/comment.controller.js';
 
 const router = express.Router();
 
 router.post('/create', verifyToken, createComment);
+router.get('/getPostComments/:postId', getPostComments);
 
 
 export default router;
