e245602ec7a3722d145095dc3340bdf24d202121
Daniel Watchman <dannietortee@gmail.com>
2024-10-22 06:53:05 +0000

    add deletepost API route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { verifyToken } from '../utils/verifyUser.js'
-import { create, getposts } from "../controllers/post.controller.js";
+import { create, getposts, deletepost } from "../controllers/post.controller.js";
 
 const router = express.Router();
 
