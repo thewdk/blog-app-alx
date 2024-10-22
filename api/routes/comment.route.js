3e332b07d36b6897eccf76a43ae331ca472bfcb6
Daniel Watchman <dannietortee@gmail.com>
2024-10-22 21:13:55 +0000

    add like

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,13 +1,16 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
 import {
-  createComment, getPostComments
+  createComment, getPostComments, likeComment, editComment
 } from '../controllers/comment.controller.js';
 
 const router = express.Router();
 
 router.post('/create', verifyToken, createComment);
 router.get('/getPostComments/:postId', getPostComments);
+router.put('/likeComment/:commentId', verifyToken, likeComment);
+router.put('/editComment/:commentId', verifyToken, editComment);
+
 
 
 export default router;
--- client/src/components/ThemeProvider.jsx
+++ client/src/components/ThemeProvider.jsx
@@ -6,7 +6,7 @@ export default function ThemeProvider({children}) {
     const {theme} = useSelector(state => state.theme)
   return (
     <div className={theme}>
-        <div className="bg-white text-gray-700 dark:text-white-700 dark:bg-[rgb(16,23,42)]  min-h-screen">
+        <div className="bg-white text-gray-700 dark:text-white dark:bg-[rgb(16,23,42)]  min-h-screen">
 
       {children}
         </div>
