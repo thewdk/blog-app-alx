15f28c1db43e20604c0be30f108d7a5653a9f1df
Daniel Watchman <dannietortee@gmail.com>
2024-10-22 21:17:26 +0000

    add edit comment

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -70,6 +70,13 @@ export default function PostPage() {
         dangerouslySetInnerHTML={{ __html: post && post.content }}
       ></div>
       <CommentSection postId={post._id} />
+      <div className='flex flex-col justify-center items-center mb-5'>
+        <h1 className='text-xl mt-5'>Recent articles</h1>
+        <div className='flex flex-wrap gap-5 mt-5 justify-center'>
+          {recentPosts &&
+            recentPosts.map((post) => <PostCard key={post._id} post={post} />)}
+        </div>
+      </div>
     </main>
   
   )
