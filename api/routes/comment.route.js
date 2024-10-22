8637100454f95f92e80bba1308adb99a061e2edb
Daniel Watchman <dannietortee@gmail.com>
2024-10-22 21:21:26 +0000

    add delete comment functionality

--- api/routes/comment.route.js
+++ api/routes/comment.route.js
@@ -1,7 +1,7 @@
 import express from 'express';
 import { verifyToken } from '../utils/verifyUser.js';
 import {
-  createComment, getPostComments, likeComment, editComment
+  createComment, getPostComments, likeComment, editComment, deleteComment
 } from '../controllers/comment.controller.js';
 
 const router = express.Router();
@@ -10,6 +10,8 @@ router.post('/create', verifyToken, createComment);
 router.get('/getPostComments/:postId', getPostComments);
 router.put('/likeComment/:commentId', verifyToken, likeComment);
 router.put('/editComment/:commentId', verifyToken, editComment);
+router.delete('/deleteComment/:commentId', verifyToken, deleteComment);
+
 
 
 
